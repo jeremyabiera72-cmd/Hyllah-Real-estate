8fb1032c5b6f77223de2a7499405f34b3039c77c
Jeremy Abiera <your-github-email@example.com>
2026-05-20 13:12:30 +0800

    Configure gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Hyllah-Real-estate/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
